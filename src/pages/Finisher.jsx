7227c58472a41827f0a945e2799933d6f5c6ec10
Deepu <dpnr915@gmail.com>
2026-01-18 19:16:25 +0530

    added navigation

--- src/pages/Finisher.jsx
+++ src/pages/Finisher.jsx
@@ -1,83 +1,86 @@
 import Template from "../components/Template";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-import { Box, Button} from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 
 
-const Finisher = ()=>{
+const Finisher = () => {
+    const navigate = useNavigate();
     return (
         <Template
-        title="Finishing up"
-        subtitle="Double-check everything looks OK before confirming."
-      >
+            title="Finishing up"
+            subtitle="Double-check everything looks OK before confirming."
+        >
 
-        <Box sx={{backgroundColor:"hsl(218, 100%, 97%)", borderRadius:"10px"}} mt={6} p={2}>
-            <Box sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}} p={2}>
-                <Box>
-                    <Typography sx={{fontWeight:"600"}} color="hsl(213, 96%, 18%)">
-                        Arcade(Monthly)
-                    </Typography>                   
+            <Box sx={{ backgroundColor: "hsl(218, 100%, 97%)", borderRadius: "10px" }} mt={6} p={2}>
+                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} p={2}>
+                    <Box>
+                        <Typography sx={{ fontWeight: "600" }} color="hsl(213, 96%, 18%)">
+                            Arcade(Monthly)
+                        </Typography>
 
-<Link to="/plan" style={{ textDecoration: 'underline', textDecorationColor: 'hsl(231, 11%, 63%)' }}>
-  <Box sx={{ color: 'hsl(231, 11%, 63%)' }}>Change</Box>
-</Link>
+                        <Link to="/plan" style={{ textDecoration: 'underline', textDecorationColor: 'hsl(231, 11%, 63%)' }}>
+                            <Box sx={{ color: 'hsl(231, 11%, 63%)' }}>Change</Box>
+                        </Link>
 
+                    </Box>
+                    <Typography sx={{ fontWeight: "600" }} color="hsl(213, 96%, 18%)">
+                        $9/mo
+                    </Typography>
                 </Box>
-                <Typography sx={{fontWeight:"600"}} color="hsl(213, 96%, 18%)">
-                       $9/mo
-                    </Typography>
-            </Box>
-            <Divider />
+                <Divider />
 
-            <Box mt={2} sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}} px={2} py={.5}>
-                <Box>
-                    <Typography   sx={{ color: 'hsl(231, 11%, 63%)' }}>
-                        Online Service
+                <Box mt={2} sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} px={2} py={.5}>
+                    <Box>
+                        <Typography sx={{ color: 'hsl(231, 11%, 63%)' }}>
+                            Online Service
+                        </Typography>
+
+                    </Box>
+                    <Typography color="hsl(213, 96%, 18%)">
+                        +$1/mo
                     </Typography>
-                
                 </Box>
-                <Typography color="hsl(213, 96%, 18%)">
-                       +$1/mo
-                    </Typography>
-            </Box>
 
-            <Box  sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}} px={2} py={.5}>
-                <Box>
-                    <Typography  sx={{ color: 'hsl(231, 11%, 63%)' }}>
-                        Larger storage
+                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} px={2} py={.5}>
+                    <Box>
+                        <Typography sx={{ color: 'hsl(231, 11%, 63%)' }}>
+                            Larger storage
+                        </Typography>
+
+                    </Box>
+                    <Typography color="hsl(213, 96%, 18%)">
+                        +$2/mo
                     </Typography>
-               
                 </Box>
-                <Typography color="hsl(213, 96%, 18%)">
-                       +$2/mo
-                    </Typography>
+
+
             </Box>
-           
-
-        </Box>
 
 
-        <Box  sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}} p={4}>
+            <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} p={4}>
                 <Box>
-                    <Typography  sx={{ color: 'hsl(231, 11%, 63%)' }}>
+                    <Typography sx={{ color: 'hsl(231, 11%, 63%)' }}>
                         Total(per month)
                     </Typography>
-               
+
                 </Box>
-                <Typography  sx={{ color: ' hsl(243, 100%, 62%)', fontWeight:"900" , fontSize:"20px"}}>
-                       +$12/mo
+                <Typography sx={{ color: ' hsl(243, 100%, 62%)', fontWeight: "900", fontSize: "20px" }}>
+                    +$12/mo
                 </Typography>
             </Box>
 
 
 
-            <Box  mt={10} sx={{display:"flex", justifyContent:"space-between"}}>
-        <Button sx={{ color: 'hsl(231, 11%, 63%)', fontWeight:"800" }}>Go Back</Button>
-        <Button variant="contained" sx={{padding:"7px 20px", backgroundColor:'hsl(243, 100%, 62%)'}}>Confirm</Button>
-    </Box>
-         </Template>
+            <Box mt={10} sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Button
+                    onClick={() => navigate("/addons")}
+                    sx={{ color: 'hsl(231, 11%, 63%)', fontWeight: "800" }}>Go Back</Button>
+                <Button variant="contained" sx={{ padding: "7px 20px", backgroundColor: 'hsl(243, 100%, 62%)' }}>Confirm</Button>
+            </Box>
+        </Template>
     )
 
 }
--- src/pages/PlanSelection.jsx
+++ src/pages/PlanSelection.jsx
@@ -9,17 +9,18 @@ import CustomSwitch from "../components/CustomSwitch";
 import { useNavigate } from "react-router-dom";
 
 
-const PlanSelection = ({plan, handleClick, selectedItem,handleCardClick}) => {
+const PlanSelection = ({ plan, handleClick, selectedItem, handleCardClick }) => {
     const navigate = useNavigate();
 
-const handleSubmit = ()=>{
-    navigate("/addons")
-}
+    const handleSubmit = (e) => {
+        console.log(e.target, "<----------------------------e")
+        navigate("/addons")
+    }
 
 
     const label = { inputProps: { 'aria-label': 'Switch' } };
     // const [plan,setPlan] = useState(false);
-    
+
 
     // Define the plans data
     const plansData = [
@@ -50,66 +51,71 @@ const handleSubmit = ()=>{
     ];
 
     return (
-        <Template 
-            title="Select your plan" 
+        <Template
+            title="Select your plan"
             subtitle="You have the option of monthly or yearly billing."
         >
             {/* <CustomTextField label="Name" placeholder="e.g. Stephen King"  ppts={{marginTop:"40px"}}/>
             <CustomTextField label="Email Address" placeholder="e.g. stephenking@lorem.com"/>
             <CustomTextField label="Phone Number" placeholder="e.g. +1 234 567 890"/> */}
-            
+
             <Stack direction="row" gap={2} justifyContent="center" marginTop={4}>
                 {plansData.map((planItem) => (
-                    <CustomCard 
+                    <CustomCard
                         key={planItem.id}
-                        selected={selectedItem===planItem.id}
-                        src={planItem.src} 
-                        title={planItem.title} 
-                        price={plan ? planItem.yearlyPrice : planItem.monthlyPrice} 
+                        selected={selectedItem === planItem.id}
+                        src={planItem.src}
+                        title={planItem.title}
+                        price={plan ? planItem.yearlyPrice : planItem.monthlyPrice}
                         addText={plan ? planItem.addText : ''}
                         onClick={() => handleCardClick(planItem.id)}
                     />
                 ))}
             </Stack>
 
-            <Box 
-            display="flex" 
-            justifyContent="center" 
-            alignItems="center" 
-            margin="50px 10px" 
-            padding="5px 0px" 
-            sx={{
-                borderRadius:"10px", 
-                backgroundColor:"hsl(218, 100%, 97%)",
-                '& .MuiSwitch-thumb': {
-            backgroundColor: '#fff !important', // Keep the same color as unchecked
-            // opacity: 1,
-          },
-          '&.Mui-checked': {
-            backgroundColor: 'red !important', // Keep the same color as unchecked
-            // opacity: 0,
-        },
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                margin="50px 10px"
+                padding="5px 0px"
+                sx={{
+                    borderRadius: "10px",
+                    backgroundColor: "hsl(218, 100%, 97%)",
+                    '& .MuiSwitch-thumb': {
+                        backgroundColor: '#fff !important', // Keep the same color as unchecked
+                        // opacity: 1,
+                    },
+                    '&.Mui-checked': {
+                        backgroundColor: 'red !important', // Keep the same color as unchecked
+                        // opacity: 0,
+                    },
                 }}>
-                <Typography sx={{ color: plan ? "hsl(231, 11%, 63%)" : "hsl(213, 96%, 18%)", fontWeight:"800" }}>Monthly</Typography>
-                <CustomSwitch {...label}  checked={plan} onChange={handleClick}/>
-                <Typography sx={{ color: plan ? "hsl(213, 96%, 18%)" : "hsl(231, 11%, 63%)", fontWeight:"800" }}>Yearly</Typography>
+                <Typography sx={{ color: plan ? "hsl(231, 11%, 63%)" : "hsl(213, 96%, 18%)", fontWeight: "800" }}>Monthly</Typography>
+                <CustomSwitch {...label} checked={plan} onChange={handleClick} />
+                <Typography sx={{ color: plan ? "hsl(213, 96%, 18%)" : "hsl(231, 11%, 63%)", fontWeight: "800" }}>Yearly</Typography>
             </Box>
 
 
-            <Box  mt={13.4} sx={{display:"flex", justifyContent:"space-between"}}>
-            <Button sx={{ color: 'hsl(231, 11%, 63%)', fontWeight:"800" ,  "&:hover": {
-                    color:"hsl(213, 96%, 18%)",
-                    backgroundColor:"#fff"
-                },}}>Go Back</Button>
-            <Button variant="contained" 
-            onClick={handleSubmit}
-            sx={{
-                "&:hover": {
-                    backgroundColor: "hsl(214, 86.70%, 32.50%)"
-                },
-                padding:"7px 20px", 
-                backgroundColor:"hsl(213, 96%, 18%)"}}>Next Step</Button>
-    </Box>
+            <Box mt={13.4} sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Button
+                    onClick={() => navigate("/")}
+                    sx={{
+                        color: 'hsl(231, 11%, 63%)', fontWeight: "800", "&:hover": {
+                            color: "hsl(213, 96%, 18%)",
+                            backgroundColor: "#fff"
+                        },
+                    }}>Go Back</Button>
+                <Button variant="contained"
+                    onClick={handleSubmit}
+                    sx={{
+                        "&:hover": {
+                            backgroundColor: "hsl(214, 86.70%, 32.50%)"
+                        },
+                        padding: "7px 20px",
+                        backgroundColor: "hsl(213, 96%, 18%)"
+                    }}>Next Step</Button>
+            </Box>
         </Template>
     )
 }
