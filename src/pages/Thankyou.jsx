9cf879c8210f9cfba4d584a1d83578079247b7a2
Deepu <dpnr915@gmail.com>
2026-01-19 11:46:57 +0530

    Enhanced Thank You page with icon and subscription confirmation message

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -1,7 +1,27 @@
+import { Box, Typography } from "@mui/material";
+import thankYouIcon from "../assets/images/icon-thank-you.svg";
+
+
 const Thankyou = () => {
 
     return (
-        <Box>
+        <Box sx={{height:"100%", color:"black", display:"flex",flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+            <img src={thankYouIcon} width="60px" height="60px" alt="thank you" />
+            <Typography
+                variant="h4"
+                gutterBottom
+                sx={{ fontWeight: 600,mt:4 }}
+                >
+                    Thank you!
+            </Typography>
+
+            <Typography
+                variant="subtitle"
+                gutterBottom
+                sx={{ mx:6, textAlign:"center", color:"hsl(231, 11%, 63%)"}}
+                >
+                Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
+            </Typography>
 
 
         </Box>
