e2a2ebfba02e0ff4ed0130fdb8d373b3d36a80da
Deepu <dpnr915@gmail.com>
2026-01-24 21:10:31 +0530

    added mobile responsiveness

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -4,52 +4,54 @@ import PropTypes from 'prop-types';
 
 
 
-const CustomCard = ({title,src,price, addText='',onClick,selected}) => {
+const CustomCard = ({ title, src, price, addText = '', onClick, selected }) => {
 
-    
+
 
   return (
     <Card
-    tabIndex={0}  // makes the card focusable
-    onClick={onClick}
-    // data-active={ }
-    sx={{
-      width: 130,
-      height: "170px",
-      padding: "2px 10px",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-around",
-      border: selected?"1px solid purple":"1px solid hsl(231, 11%, 63%)", // default border
-      backgroundColor:selected?"hsl(218, 100%, 97%)":"white",
-      borderRadius: "8px",
-      outline: "none", // prevent default focus ring   
-      cursor: "pointer",
-      "&:hover, &:focus-visible":{
-        borderColor: "purple",
-        backgroundColor: "hsl(218, 100%, 99%)",
-        boxShadow: "0px 4px 12px rgba(0,0,0,0.12)", 
-        transition: "all 0.3s ease-in-out",
-      }
-      
-    }}
-  >
-    <img src={src} height="40px" width="40" />
-    <Box sx={{ marginTop: "10px" }}>
-      <Typography fontWeight="700" sx={{color:"hsl(213, 96%, 18%)"}}  >{title}</Typography>
-      <Typography sx={{color:"hsl(231, 11%, 63%)"}}>${price}</Typography>
-      {addText && <Typography color="hsl(213, 96%, 18%)">{addText}</Typography>}
-    </Box>
-  </Card>
+      tabIndex={0}  // makes the card focusable
+      onClick={onClick}
+      // data-active={ }
+      sx={{
+        width: { xs: "90%", lg: 120 },
+        height: { xs: "auto", lg: "170px" },
+        padding: { xs: "10px 10px", lg: "2px 10px" },
+        display: "flex",
+        flexDirection: { xs: "row", lg: "column" },
+        justifyContent: { xs: "start", lg: "space-around" },
+        alignItems: { xs: "center", lg: "start" },
+        gap: { xs: 2, lg: 0 },
+        border: selected ? "1px solid purple" : "1px solid hsl(231, 11%, 63%)", // default border
+        backgroundColor: selected ? "hsl(218, 100%, 97%)" : "white",
+        borderRadius: "8px",
+        outline: "none", // prevent default focus ring   
+        cursor: "pointer",
+        "&:hover, &:focus-visible": {
+          borderColor: "purple",
+          backgroundColor: "hsl(218, 100%, 99%)",
+          boxShadow: "0px 4px 12px rgba(0,0,0,0.12)",
+          transition: "all 0.3s ease-in-out",
+        }
+
+      }}
+    >
+      <img src={src} height="40px" width="40" />
+      <Box sx={{ marginTop: { xs: "none", md: "10px" } }}>
+        <Typography fontWeight="700" sx={{ color: "hsl(213, 96%, 18%)" }}  >{title}</Typography>
+        <Typography sx={{ color: "hsl(231, 11%, 63%)" }}>${price}</Typography>
+        {addText && <Typography color="hsl(213, 96%, 18%)">{addText}</Typography>}
+      </Box>
+    </Card>
   )
 }
 
 CustomCard.propTypes =
 {
-    title: PropTypes.string.isRequired,
-    src: PropTypes.string.isRequired,
-    addText: PropTypes.string,
-    price:PropTypes.number.isRequired,    
+  title: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  addText: PropTypes.string,
+  price: PropTypes.number.isRequired,
 }
 
 export default CustomCard
