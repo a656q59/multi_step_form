bdf7e76ab45a2dd01167949262eefd8ad7ca863a
Deepu <dpnr915@gmail.com>
2026-01-24 21:11:41 +0530

    added mobile responsive layout

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,32 +1,32 @@
 
 
-import {  Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import PropTypes from "prop-types";
 
 
 
-const Template = ({title,subtitle, children})=>{
+const Template = ({ title, subtitle, children }) => {
 
     return (
-        <Grid container >
-        <Box  height="79px">
-        <Typography  sx={{fontWeight:"800", fontSize:"34px", color:"hsl(213, 96%, 18%)"}}>{title}</Typography>
-        <Typography variant="body1" sx={{opacity:".5",color:" hsl(231, 11%, 63%)"}}>{subtitle}</Typography>
-       </Box>
-       <Box  width="100%" height="364px">
-       {children}
-       </Box>       
-       <Box  width="100%"height="110px">
-       </Box>
-       </Grid>
+        <Grid container sx={{ border: "2px solid green", padding: { xs: "20px" } }}>
+            <Box height="79px">
+                <Typography sx={{ fontWeight: "800", fontSize: "34px", color: "hsl(213, 96%, 18%)" }}>{title}</Typography>
+                <Typography variant="body1" sx={{ opacity: ".5", color: " hsl(231, 11%, 63%)" }}>{subtitle}</Typography>
+            </Box>
+            <Box width="100%" height="364px">
+                {children}
+            </Box>
+            <Box width="100%" height="110px">
+            </Box>
+        </Grid>
     );
 }
 
 
-Template.propTypes ={
+Template.propTypes = {
     title: PropTypes.string.isRequired,
-    subtitle:PropTypes.string.isRequired,
-    children:PropTypes.node.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 
