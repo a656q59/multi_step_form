d6461f1226c3c3547c814319bae22ae2441bdeea
Deepu <dpnr915@gmail.com>
2026-01-25 14:15:21 +0530

    conditional rendering added tp navigation buttons for various screen sizes

--- src/pages/PersonalInfo.jsx
+++ src/pages/PersonalInfo.jsx
@@ -1,4 +1,4 @@
-import {  Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import CustomTextField from "../components/CustomTextField";
 import Template from "../components/Template";
 import { useState } from "react";
@@ -7,51 +7,51 @@ import { useNavigate } from "react-router-dom";
 
 
 
-const PersonalInfo = ()=>{
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        phone: "",
-      });
-      
-      const [errors, setErrors] = useState({
-        name: false,
-        email: false,
-        phone: false,
-      });
-      
-      const handleChange = (field) => (e) => {
-        setFormData({
-          ...formData,
-          [field]: e.target.value,
-        });
-      
-        // clear error as user types
-        setErrors({
-          ...errors,
-          [field]: false,
-        });
-      };
+const PersonalInfo = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    phone: false,
+  });
+
+  const handleChange = (field) => (e) => {
+    setFormData({
+      ...formData,
+      [field]: e.target.value,
+    });
+
+    // clear error as user types
+    setErrors({
+      ...errors,
+      [field]: false,
+    });
+  };
 
 
-      const handleSubmit = () => {
-        console.log("handleSubmit clicked")
-        const newErrors = {
-          name: formData.name.trim() === "",
-          email: formData.email.trim() === "",
-          phone: formData.phone.trim() === "",
-        };
-      
-        setErrors(newErrors);
-      
-        if (Object.values(newErrors).some(Boolean)) return;
-      
-        navigate("/plan")
-      };
+  const handleSubmit = () => {
+    console.log("handleSubmit clicked")
+    const newErrors = {
+      name: formData.name.trim() === "",
+      email: formData.email.trim() === "",
+      phone: formData.phone.trim() === "",
+    };
+
+    setErrors(newErrors);
+
+    if (Object.values(newErrors).some(Boolean)) return;
+
+    navigate("/plan")
+  };
 
 
-    return (
+  return (
     //    <Grid container border="2px solid green">
     //     <Box border="2px solid red">
     //     <Typography  sx={{fontWeight:"800", fontSize:"34px"}}>Personal info</Typography>
@@ -62,55 +62,58 @@ const PersonalInfo = ()=>{
     //    <CustomTextField label="Email Address" placeholder="e.g. stephenking@lorem.com"/>
     //    <CustomTextField label="Phone Number" placeholder="e.g. +1 234 567 890"/>
     //    </Box>
-       
+
     //    <Box border="2px solid red" width="100%"height="110px"></Box>
     //    </Grid>
 
-    <Template 
-    title="Personal info" 
-    subtitle="Please provide your name, email address, and phone number."
+    <Template
+      title="Personal info"
+      subtitle="Please provide your name, email address, and phone number."
     >
       <CustomTextField
-  label="Name"
-  placeholder="e.g. Stephen King"
-  ppts={{ marginTop: "40px" }}
-  value={formData.name}
-  handleChange={handleChange("name")}
-  error={errors.name}
-/>
+        label="Name"
+        placeholder="e.g. Stephen King"
+        ppts={{ marginTop: "40px" }}
+        value={formData.name}
+        handleChange={handleChange("name")}
+        error={errors.name}
+      />
 
-<CustomTextField
-  label="Email Address"
-  placeholder="e.g. stephenking@lorem.com"
-  value={formData.email}
-  handleChange={handleChange("email")}
-  error={errors.email}
-/>
+      <CustomTextField
+        label="Email Address"
+        placeholder="e.g. stephenking@lorem.com"
+        value={formData.email}
+        handleChange={handleChange("email")}
+        error={errors.email}
+      />
 
-<CustomTextField
-  label="Phone Number"
-  placeholder="e.g. +1 234 567 890"
-  value={formData.phone}
-  handleChange={handleChange("phone")}
-  error={errors.phone}
-/>
+      <CustomTextField
+        label="Phone Number"
+        placeholder="e.g. +1 234 567 890"
+        value={formData.phone}
+        handleChange={handleChange("phone")}
+        error={errors.phone}
+      />
 
-        <Box  mt={8.7} sx={{display:"flex", justifyContent:"end"}}>
-           
-            <Button variant="contained" 
-            onClick={handleSubmit}
-            sx={{
-                "&:hover": {
-                    backgroundColor: "hsl(214, 86.70%, 32.50%)"
-                },
-                padding:"7px 20px", 
-                backgroundColor:"hsl(213, 96%, 18%)"}}>Next Step</Button>
-    </Box>
+
+      {/* ------------------------------navigation buttons------------------------------------- */}
+      <Box mt={8.7} display={{ xs: "none", lg: "flex" }} sx={{ justifyContent: "end" }}>
+
+        <Button variant="contained"
+          onClick={handleSubmit}
+          sx={{
+            "&:hover": {
+              backgroundColor: "hsl(214, 86.70%, 32.50%)"
+            },
+            padding: "7px 20px",
+            backgroundColor: "hsl(213, 96%, 18%)"
+          }}>Next Step</Button>
+      </Box>
 
 
 
     </Template>
-    )
+  )
 
 }
 
