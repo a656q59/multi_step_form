20cde8fce4d1772385ccda2dcc742ca4045f40b3
Deepu <dpnr915@gmail.com>
2026-01-26 17:32:09 +0530

    fixed alignment issues

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -1,30 +1,33 @@
 import { Box, Typography } from "@mui/material";
 import thankYouIcon from "../assets/images/icon-thank-you.svg";
+import Template from "../components/Template";
 
 
 const Thankyou = () => {
 
     return (
-        <Box sx={{height:"100%", color:"black", display:"flex",flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
-            <img src={thankYouIcon} width="60px" height="60px" alt="thank you" />
-            <Typography
-                variant="h4"
-                gutterBottom
-                sx={{ fontWeight: 600,mt:4 }}
+        <Template>
+            <Box sx={{ height: "100%", color: "black", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", }}>
+                <img src={thankYouIcon} width="60px" height="60px" alt="thank you" />
+                <Typography
+                    variant="h4"
+                    gutterBottom
+                    sx={{ fontWeight: 600, mt: 4 }}
                 >
                     Thank you!
-            </Typography>
+                </Typography>
 
-            <Typography
-                variant="subtitle"
-                gutterBottom
-                sx={{ mx:6, textAlign:"center", color:"hsl(231, 11%, 63%)"}}
+                <Typography
+                    variant="subtitle"
+                    gutterBottom
+                    sx={{ mx: 6, textAlign: "center", color: "hsl(231, 11%, 63%)" }}
                 >
-                Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
-            </Typography>
+                    Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
+                </Typography>
 
 
-        </Box>
+            </Box>
+        </Template>
     )
 }
 export default Thankyou; 
